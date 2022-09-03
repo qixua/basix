53dd602e72b64e448b3f5f55193cbaaa3907be34
qixua <qixua@proton.me>
2022-09-03 20:05:38 +0300

    fix: script messing up when there is no \n

--- source/utils/process_keymap.py
+++ source/utils/process_keymap.py
@@ -7,7 +7,8 @@ def process_keymap(abs_path: str) -> list[dict]:
         # to remove the \n at the end of every line
         # not len(l) - 2 as len() as 1-based
         for i, l in enumerate(keymap_lines):
-            keymap_lines[i] = l[: len(l) - 1]
+            if "\n" in keymap_lines[i]:
+                keymap_lines[i] = l[: len(l) - 1]
 
         for l in keymap_lines:
             keymap.append({"c": l[:1], "v": l[5:]})
