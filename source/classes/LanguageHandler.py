cc88212acba2d73541101d660953d633d821c8a9
qixua <qixua@proton.me>
2022-09-06 01:05:55 +0300

    perf: format_language
    Use join() instead of a for loop

--- source/classes/LanguageHandler.py
+++ source/classes/LanguageHandler.py
@@ -1,18 +1,10 @@
 class LanguageHandler:
-    def __init__(self, languages, sections):
+    def __init__(self, languages: list[str], sections: dict[list]):
         self.languages = languages
         self.sections = sections
 
-    def format_languages(self):
-        formatted = ""
-
-        for lang in self.languages:
-            if lang != self.languages[-1]:
-                formatted += f"{lang}/"
-            else:
-                formatted += lang
-
-        return f"[{formatted}]"
+    def format_languages(self, sep):
+        return f"[{sep.join(self.languages)}]"
 
     def get_lang_and_text(self):
         lang_options = self.format_languages()
