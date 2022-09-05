1132e12a143ff63d7fc0f8d6ddfbf5d8da4028b3
qixua <qixua@proton.me>
2022-09-06 00:06:05 +0300

    feat: LanguageHandler class

---
+++ source/classes/LanguageHandler.py
@@ -0,0 +1,24 @@
+class LanguageHandler:
+    def __init__(self, languages, sections):
+        self.languages = languages
+        self.sections = sections
+
+    def format_languages(self):
+        formatted = ""
+
+        for i, lang in enumerate(self.languages):
+            if lang != self.languages[-1]:
+                formatted += f"{lang}/"
+            else:
+                formatted += lang
+
+        return f"[{formatted}]"
+
+    def get_lang_and_text(self):
+        lang_options = self.format_languages()
+        self.language = ""
+
+        while self.language not in self.languages:
+            self.language = input(f"{lang_options} ? ")
+
+        return self.sections[self.language]
